a3bb6db8c91d789a6f6b1c523cb9d63add6b220a
Tulitu Vlad <tulituvlad@gmail.com>
2023-02-06 17:34:41 +0200

    ui update

--- app/src/main/java/com/theviciousgames/refreshratechecker/MainActivity.kt
+++ app/src/main/java/com/theviciousgames/refreshratechecker/MainActivity.kt
@@ -2,10 +2,22 @@ package com.theviciousgames.refreshratechecker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        forceDayMode()
+        keepScreenAwake()
+    }
+
+    private fun forceDayMode() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+
+    private fun keepScreenAwake() {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 }
---
+++ app/src/main/java/com/theviciousgames/refreshratechecker/ui/welcome/view/FirstOnBoardingFragment.kt
@@ -0,0 +1,37 @@
+package com.theviciousgames.refreshratechecker.ui.welcome.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.theviciousgames.refreshratechecker.R
+import com.theviciousgames.refreshratechecker.databinding.FragmentOnboardingBinding
+import com.theviciousgames.refreshratechecker.ui.welcome.viewmodel.OnBoardingViewModel
+
+class FirstOnBoardingFragment : Fragment(R.layout.fragment_onboarding) {
+    private var _binding: FragmentOnboardingBinding? = null
+    private val viewModel: OnBoardingViewModel by viewModels()
+    private val binding: FragmentOnboardingBinding
+        get() = _binding!!
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun updateUi() {
+        with(binding)
+        {
+            animationView.cancelAnimation()
+            textviewTitle.text = "Change your DPI!"
+            textviewDescription.text =
+                "You either want to be a great gamer with a better aim or you simply want a zoomed-out screen and you came to the right place!\n Lets us know in a review what's your opinion!"
+
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        updateUi()
+    }
+}
